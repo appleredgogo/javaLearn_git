e1700417cba1fd1a5441a034e77a649694f344f8
appleredgogo <2386284963@qq.com>
2021-08-15 21:30:36 +0800

    第二周课后作业(第六题)

---
+++ week2/nio/HttpClient.java
@@ -0,0 +1,51 @@
+package java0.nio1.apache;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * HttpClient
+ * @author shizeyu
+ * @date 2021/8/15-19:31
+ */
+public class HttpClient {
+    public static void main(String[] args) throws IOException {
+
+        getHttpClient();
+    }
+
+    public static void getHttpClient(){
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //声明get请求
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+        //发送请求
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            //判断状态码
+            if(response.getStatusLine().getStatusCode()==200){
+                HttpEntity entity = response.getEntity();
+                //使用工具类EntityUtils，从响应中取出实体表示的内容并转换成字符串
+                String string = EntityUtils.toString(entity, "utf-8");
+                System.out.println(string);
+            }
+        }catch (IOException e){
+            System.out.println("连接失败！！"+e.getMessage());
+        }finally {
+            try {
+                //5.关闭资源
+                response.close();
+                httpClient.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ week2/nio/OkHttp.java
@@ -0,0 +1,39 @@
+package java0.nio1.apache;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * OkHttp
+ * @author shizeyu
+ * @date 2021/8/15-20:16
+ */
+public class OkHttp {
+    public static void main(String[] args) throws IOException {
+        getOkHttp();
+    }
+    public static void getOkHttp(){
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        String url = String.format("http://%s:%s/", "localhost", "8801");
+        Request request = new Request.Builder().url(url).get().build();
+        Call call = null;
+        Response response = null;
+        try {
+            call = okHttpClient.newCall(request);
+            response = call.execute();
+            System.out.println(response.body().string());
+        }catch (IOException e){
+            System.out.println("连接失败！！"+e.getMessage());
+        }finally {
+            //5.关闭资源
+            response.close();
+            response.body().close();
+        }
+
+    }
+}
